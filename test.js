2158a127e2ee48c64d82572505f67d427fbd6dbc
yeshey choden <yeshey@DESKTOP-ES2KOVM.localdomain>
2022-10-26 01:17:58 +0200

    my profile page

---
+++ test.js
@@ -0,0 +1,45 @@
+<div class="column">
+  <div class="left">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVmrqULHeEiafOXS8F8Ph49eWNY65MuX-B6g&usqp=CAU">
+    <h4>Name : Yeshey Choden</h4>  
+    <h4>Nationality: Bhutanese</h4>  
+    <h4>D.O.B: 05/04/1996</h4>  
+    <h4><em>Objectives</em></h4>
+    <ul>
+      <li>Learn web programming</li>
+      <li>Practice some excercises daily</li>
+      <li>Team building </li>
+      <li>Network and connections </li>
+      
+    </ul>
+
+  </div>
+  <div class="right">
+    <h3>Yeshey choden</h3>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVmrqULHeEiafOXS8F8Ph49eWNY65MuX-B6g&usqp=CAU" alt="Avatar">
+    <h5>e-mail: yeshey choden</h5>
+    <h5>contact no.: +39 3458150913</h5>
+
+  </div>*/</div>
+
+  body {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 300; /* if you want the light version */
+  }
+* {
+    box-sizing: border-box;
+  }
+.left {
+    width: 250px;
+
+  }
+  
+.right {
+    width: 750px;
+    left: 120px;
+  }
+img {
+    border-radius: 150px;
+    height: 300px;
+    width: 300px;
+  }
